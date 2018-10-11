f0a3574189407296d31811acd489dced39350b87
Kevin Deng <ktd9@duke.edu>
2018-10-10 21:43:33 -0400

    update sample

--- test-sample.sql
+++ test-sample.sql
@@ -1,4 +1,4 @@
-SELECT uid, name FROM User WHERE longitude>100 AND longitude<200 AND latitude>100 AND latitude<200;
+SELECT uid, name FROM User WHERE longitude>50 AND longitude<80 AND latitude>50 AND latitude<80;
 SELECT name FROM Ingredients WHERE name='tomato' or name='cheese';
 SELECT url, name FROM Recipe WHERE name LIKE 'tomato%';
 
