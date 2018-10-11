430c9eb9b4490da3e978112a2a7e33d79faf7362
Diane Hu <diane.hu@duke.edu>
2018-10-10 22:26:21 -0400

    Update test-sample.sql

--- test-sample.sql
+++ test-sample.sql
@@ -1,4 +1,4 @@
-SELECT uid, name FROM User WHERE longitude>50 AND longitude<80 AND latitude>50 AND latitude<80;
+SELECT uid, name FROM Users WHERE longitude>50 AND longitude<80 AND latitude>50 AND latitude<80;
 SELECT name FROM Ingredients WHERE name='tomato' or name='cheese';
-SELECT url, name FROM Recipe WHERE name LIKE 'tomato%';
+SELECT url, name FROM Recipe WHERE name LIKE 'Noodle%';
 
