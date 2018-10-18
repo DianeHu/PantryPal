0a0a3e1ada8073cec4ee770e0b3103bfa01455d9
Diane Hu <dh224@duke.edu>
2018-10-17 21:59:31 -0400

    Deployed

---
+++ backend/main.py
@@ -0,0 +1,15 @@
+from flask import (
+    render_template
+)
+import connexion
+
+app = connexion.App(__name__, specification_dir='./')
+app.add_api('swagger.yml')
+
+@app.route("/")
+def main():
+    return render_template('temp.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
