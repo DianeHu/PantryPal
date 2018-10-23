c259a5a9198c5006f1eb102b0f6c6a0588617035
Diane Hu <dh224@duke.edu>
2018-10-22 20:24:53 -0400

    Not working connection to sql

--- create.sql
+++ create.sql
@@ -1,26 +1,30 @@
-CREATE TABLE User
+CREATE TABLE Users
 (uid INT NOT NULL PRIMARY KEY,
  latitude NUMERIC NOT NULL CHECK(latitude <= 90 AND latitude >= -90),
  longitude NUMERIC NOT NULL CHECK(longitude <= 180 AND longitude >= -180),
- name VARCHAR(256) NOT NULL
+ name VARCHAR(256) NOT NULL,
+ email VARCHAR(256) NOT NULL
  );
 
 CREATE TABLE Ingredients
-(name VARCHAR(256) NOT NULL,
-measurement VARCHAR(256) NOT NULL,
-price_per_unit NUMERIC NOT NULL,
-PRIMARY KEY (name)
+(name VARCHAR(256) NOT NULL PRIMARY KEY
 );
 
-CREATE TABLE UserPantry(
+CREATE TABLE Owns
+(uid INT NOT NULL REFERENCES Users(uid),
+ ingredient VARCHAR(256) NOT NULL REFERENCES Ingredients(name),
+ PRIMARY KEY(uid, ingredient)
+);
+
+/*CREATE TABLE UserPantry(
 uid INT NOT NULL,
 pantryid INT NOT NULL,
 PRIMARY KEY(uid),
 PRIMARY KEY(pantryid),
 FOREIGN KEY uid REFERENCES user(uid)
-);
+);*/
 
-CREATE TABLE PantryIngredients(
+/*CREATE TABLE PantryIngredients(
 pantryid INT NOT NULL,
 ingredient_name VARCHAR(256) NOT NULL,
 quantity NUMERIC NOT NULL,
@@ -28,12 +32,12 @@ PRIMARY KEY pantryid,
 PRIMARY KEY ingredient_name,
 FOREIGN KEY ingredient_name REFERENCES Ingredients(name),
 FOREIGN KEY pantryid REFERENCES UserPantry(pantryid)
-);
+);*/
 
-CREATE TABLE Recipe(
-	-- Recipe's key is changed to be a name 
+/*CREATE TABLE Recipe(
+	-- Recipe's key is changed to be a name
 name VARCHAR(256) NOT NULL,
 url VARCHAR(256) NOT NULL,
 PRIMARY KEY name,
-PRIMARY KEY url 
-);
+PRIMARY KEY url
+);*/
