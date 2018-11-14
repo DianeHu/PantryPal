8d902e7d3e15767d6250fbc980eea4911c914c0d
Jarod Cahoon <jarod.b.cahoon@gmail.com>
2018-11-14 16:10:14 -0500

    Worked on refining the return values of the endpoints a little more (with HTTP Statuses) more to come when we get the database setup and the API calls we need

--- app.py
+++ app.py
@@ -43,30 +43,45 @@ def verifyToken(token):
 
 # TODO
 # # returns recipes for a specific user
-@app.route("/getrecipes", methods=['GET', 'POST'])
+@app.route("/getrecipes", methods=['GET'])
 def getRecipes():
      user = verifyToken(request.args.get('token'))
      if user:
+         userKey = user.get('email')
+         # Gonna have to wait for the database to get the response
+         myIngredients = getIngredients(userKey)
+         responseData = jsonify(#Insert API call here) # Api Call with current Ingredients
+         return (responseData, 200)
+     else:
+         return (None, 401)
 #
 # TODO
 # # modify for a specific user
 # # input: key value pairs of ingredient, shouldBeAdded
 # # if shouldBeAdded is True, add to database, otherwise remove
 
-@app.route("/modifyingredients/<string:ingredient>/<int:shouldBeAdded>", methods=['GET', 'POST'])
+@app.route("/modifyingredients/<string:ingredient>/<int:shouldBeAdded>", methods=['POST'])
 def modifyIngredients(ingredient, shouldBeAdded):
     user = verifyToken(request.args.get('token'))
     if user:
-        if(request.method == 'POST'):
-
+        userKey = user.get('email')
+        # Posting code here, not DB not setup so not sure yet
+        response = jsonify(#tbd)
+        return (response, 201)
     else:
+        return (None, 401)
 
 # TODO
 # # return list of all ingredients
 @app.route("/getingredients", methods=['GET'])
 def getIngredients():
-     user = verifyToken(token)
+     user = verifyToken(request.args.get('token'))
      if user:
+         userKey = user.get('email')
+         myIngredients = jsonify(None) # Will have to change to make DB call when we get that set up
+         return (myIngredients, 200)
+     else:
+         return (None, 401)
 
 
 
