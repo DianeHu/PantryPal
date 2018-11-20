0d7fd3191be53de211a90f4f6c903e0822f94404
Diane Hu <dh224@duke.edu>
2018-11-19 22:03:21 -0500

    Testing

---
+++ main.py
@@ -0,0 +1,46 @@
+# Copyright 2015 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# [START app]
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
+app = Flask(__name__)
+
+# [START example]
+# Environment variables are defined in app.yaml.
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://root:pantry-password@/pantry-test?host=/cloudsql/pantrypal-316:us-east1:pantry-test"
+#os.environ['SQLALCHEMY_DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredient = db.Column(db.String(50))
+
+    def __init__(self, ingredient):
+        self.ingredient = ingredient
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'Hello World!'
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
+# [END app]
